22369b31f55f08fcbf9ac5dbd7a4dfdcc52145f2
d5ng <dong5ffice@gmail.com>
2023-06-16 18:22:41 +0900

    파일 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,41 @@
-import React, { useState } from 'react'
-import Expense from './components/Expense';
+import React from 'react'
+import Expense from './components/Expense/Expense';
 
 
-// function App() {
-//   const expenses = [
-//     {
-//       id: "e1",
-//       title: "Toilet Paper",
-//       amount: 94.12,
-//       date: new Date(2020, 7, 14),
-//     },
-//     { 
-//       id: "e2", 
-//       title: "New TV", 
-//       amount: 799.49, 
-//       date: new Date(2021, 2, 12) },
-//     {
-//       id: "e3",
-//       title: "Car Insurance",
-//       amount: 294.67,
-//       date: new Date(2021, 2, 28),
-//     },
-//     {
-//       id: "e4",
-//       title: "New Desk (Wooden)",
-//       amount: 450,
-//       date: new Date(2021, 5, 12),
-//     },
-//   ];
+function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { 
+      id: "e2", 
+      title: "New TV", 
+      amount: 799.49, 
+      date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
 
-//   return (
-//     <div>
-//       <h2>Let's get Started!!!</h2>
-//       <Expense items={expenses} />
-//     </div>
-//   );
-// }
-
-function MyComponent(){
-  const [title, setTitle] = useState('클릭 전');
-  const clickHandler = () => setTitle('클릭 후')
   return (
     <div>
-      <div>{title}</div>
-      <button onClick={clickHandler}>버튼</button>
+      <h2>Let's get Started!!!</h2>
+      <Expense items={expenses} />
     </div>
   );
 }
 
-function App(){
-  return <MyComponent />;
-}
 
 export default App
